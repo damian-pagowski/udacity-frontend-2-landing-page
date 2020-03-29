5a8e6e9a019623246265ce10861554eb0f297e21
Damian Pagowski <d.pagowski@gmail.com>
2020-03-29 23:55:25 +0700

    added listeners for page scrolling and clicking menu

--- js/app.js
+++ js/app.js
@@ -20,48 +20,64 @@
 const nav = document.getElementById("navbar__list");
 const sections = document.querySelectorAll("section");
 
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
+// Build menu
+const createMenuItem = el => {
+  let section_id = el.getAttribute("id");
+  let nav_name = el.getAttribute("data-nav");
+  let li = document.createElement("li");
+  li.innerHTML = `<a  data-link=${section_id} class="menu__link">${nav_name}</a>`;
+  li.addEventListener("click", () => scrollToSection(el));
+  nav.appendChild(li);
+};
 
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
+const removeActiveClassFromSections = () =>
+  sections.forEach(el => el.classList.remove("your-active-class"));
+
+const removeActiveClassFromMenuItems = () =>
+  document
+    .querySelectorAll(".menu__link")
+    .forEach(el => el.classList.remove("active"));
+
+// Scroll to section on link click
+const scrollToSection = elmnt => {
+  elmnt.scrollIntoView();
+};
+
+const setSectionActive = active => {
+  active.classList.add("your-active-class");
+};
+
+const setCorrespondingMenuItemActive = active => {
+  let section_id = active.getAttribute("id");
+  const menuItem = document.querySelector(`[data-link="${section_id}"]`);
+  menuItem.classList.add("active");
+};
+
+// Set section and corresponding menu item active
+const onScrollHandler = () => {
+  let active = null;
+
+  sections.forEach(elmnt => {
+    const rect = elmnt.getBoundingClientRect();
+    const dist = rect.top;
+    const treshold = 300;
+    if (dist < treshold) {
+      active = elmnt;
+    }
+  });
+
+  removeActiveClassFromSections();
+  removeActiveClassFromMenuItems();
+  if (active) {
+    setSectionActive(active);
+    setCorrespondingMenuItemActive(active);
+  }
+};
 
 // build the nav
 
 sections.forEach(el => {
-  let section_id = el.getAttribute("id");
-  let nav_name = el.getAttribute("data-nav");
-  let li = document.createElement("li");
-  li.innerHTML = `<a fref="#${section_id}" class="menu__link">${nav_name}</a>`;
-
-nav.appendChild(li);
-
-
+  createMenuItem(el);
 });
 
-// Add class 'active' to section when near top of viewport
-
-// Scroll to anchor ID using scrollTO event
-
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-
-// Scroll to section on link click
-function scrollToSection(){
-
-}
-// Set sections as active
-function setActive(){
-    
-}
+window.addEventListener("scroll", onScrollHandler);
